966500abae1d166dc2613000ee87587ee5ad1ee4
Victor Stenström-Diaz <victor.stenstrom-diaz@appliedtechnology.se>
2019-10-31 10:55:34 +0100

    first layer of weatherHandling done

---
+++ weatherhack/weatherHandler.js
@@ -0,0 +1,47 @@
+const jacketsJSON = {"jackets": [
+  {
+    "id": "denim jacket",
+    "weather": "sun",
+    "temperature": {
+      "high": "20",
+      "low": "15"
+    }
+  },
+  {
+    "id": "carhartt jacket",
+    "weather": ["sun", "rain"],
+    "temperature": {
+      "high": "16",
+      "low": "10"
+    }
+  },
+  {
+    "id": "bane jacket",
+    "weather": ["sun", "snow"],
+    "temperature": {
+      "high": "0",
+      "low": "-15"
+    }
+  },
+  {
+    "id": "grey coat",
+    "weather": ["sun", "snow"],
+    "temperature": {
+      "high": "5",
+      "low": "-12"
+    }
+  }
+]}
+
+
+// Returns an array of the clothing articles that are within temperature range 
+// needs refactoring (helper functions) and changes for clothing types other than jackets
+const comfortAlgorithm = (jsonClothingObj, currentTemperature) => {
+  const comfortCheck = jsonClothingObj.jackets.filter(article => {
+    const hiTemp = article.temperature.high
+    const lowTemp = article.temperature.low
+    return (hiTemp >= currentTemperature && currentTemperature >= lowTemp)
+  })
+  const comfortMatch = comfortCheck.map(article => article.id)
+  return comfortMatch
+}
