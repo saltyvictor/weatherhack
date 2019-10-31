b4e03e9ceff2520eba46c2dd203dc6e47882f5da
Victor Stenström-Diaz <victor.stenstrom-diaz@appliedtechnology.se>
2019-10-31 17:59:55 +0100

    implemented weather component

--- weatherhack/src/App.js
+++ weatherhack/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
-import Clothes from './components/Clothes'
+import Weather from './components/Weather/Weather'
 
 
 function App() {
   return (
     <div className="App">
       <Container className="container" maxWidth="sm">
-        <Clothes temperature/>
+        <Weather/>
       </Container>
     </div>
   );
+  // sending Weather in without props 
 }
 
 export default App;
